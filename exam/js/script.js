368fc40cdc26f096f25fc9130a336f60a50fdddc
Nehemiah A. Dagohoy <70300865+ndagohoy@users.noreply.github.com>
2020-10-13 20:08:25 +0800

    Update script.js

--- exam/js/script.js
+++ exam/js/script.js
@@ -15,3 +15,13 @@
     });
   }, false);
 })();
+
+function Login(){
+  var x = document.getElementById("inputId").value;
+  if(x == ""){
+    return false;
+  }
+  else{
+    alert('Account not registered in the admin page. Please try again later.');
+  }
+}
